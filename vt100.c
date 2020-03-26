71dc7f8112405320121aac6aa07a47f2075bcbb7
Martin <martin@ideaalab.com>
2020-03-26 18:47:01 +0100

    -se añaden funciones que permiten mover cursor, lineas o atributos con parametros

---
+++ vt100.c
@@ -0,0 +1,52 @@
+#include "vt100.h"
+
+/*
+ * Move the cursor relative to the actual position
+ * num = number of positions to move
+ * dir = direction to move (use defines ANSI_CUR_UP/ANSI_CUR_DOWN/ANSI_CUR_RIGHT/ANSI_CUR_LEFT)
+ */
+void moveCursorRelative(int num, char dir){
+	printf("\x1B[%u%c", num, dir);
+}
+
+/*
+ * Move the cursor to anywhere of the screen
+ * h = horizontal position
+ * v = vertical position
+ */
+void moveCursorAbsolute(int h, int v){
+	printf("\x1B[%u;%uH", h, v);
+}
+
+/*
+ * Moves 'num' lines up/down, and set cursor at the beginning
+ * num = number of lines to move
+ * dir = direction to move (use defines ANSI_LINE_UP/ANSI_LINE_DOWN
+ */
+void moveLine(int num, char dir){
+	printf("\x1B[%u%c", num, dir);
+}
+
+/*
+ * Select Graphic Rendition (SGR)
+ * Accept 1 value
+ */
+void SGR1(int val1){
+	printf("\x1B[%um", val1);
+}
+
+/*
+ * Select Graphic Rendition (SGR)
+ * Accept 2 values
+ */
+void SGR2(int val1, val2){
+	printf("\x1B[%u;%um", val1, val2);
+}
+
+/*
+ * Select Graphic Rendition (SGR)
+ * Accept 3 values
+ */
+void SGR3(int val1, val2, val3){
+	printf("\x1B[%u;%u;%um", val1, val2, val3);
+}
--- vt100.h
+++ vt100.h
@@ -33,45 +33,8 @@
 #define ANSI_TWE			"\x1B[?7h"	//Enable text wraps
 #define ANSI_TWD			"\x1B[?7l"	//Disable text wrap
 
-//characters
-#define ANSI_OCA			"\x1B[m"	//Turn off character attributes
-#define ANSI_BMO			"\x1B[1m"	//Turn bold mode on
-#define ANSI_LIM			"\x1B[2m"	//Turn low intensity mode on (bold off)
-#define ANSI_UM				"\x1B[4m"	//Turn underline mode on
-#define ANSI_BM				"\x1B[5m"	//Turn blinking mode on
-#define ANSI_RV				"\x1B[7m"	//Turn reverse video on
-#define ANSI_ITM			"\x1B[8m"	//Turn invisible text mode on
-
-//foreground color
-#define ANSI_F_BLACK		"\x1B[30m"	//black
-#define ANSI_F_RED			"\x1B[31m"	//red
-#define ANSI_F_GREEN		"\x1B[32m"	//green
-#define ANSI_F_YELLOW		"\x1B[33m"	//Yellow
-#define ANSI_F_BLUE			"\x1B[34m"	//Blue
-#define ANSI_F_MAGENTA		"\x1B[35m"	//Magenta
-#define ANSI_F_CYAN			"\x1B[36m"	//Cyan
-#define ANSI_F_WHITE		"\x1B[37m"	//White
-
-//background color
-#define ANSI_B_BLACK		"\x1B[40m"	//black
-#define ANSI_B_RED			"\x1B[41m"	//red
-#define ANSI_B_GREEN		"\x1B[42m"	//green
-#define ANSI_B_YELLOW		"\x1B[43m"	//Yellow
-#define ANSI_B_BLUE			"\x1B[44m"	//Blue
-#define ANSI_B_MAGENTA		"\x1B[45m"	//Magenta
-#define ANSI_B_CYAN			"\x1B[46m"	//Cyan
-#define ANSI_B_WHITE		"\x1B[47m"	//White
-
-//cursor position
-#define ANSI_CUU(n)			"\x1B[nA"	//Move cursor up n lines
-#define ANSI_CUD(n)			"\x1B[nB"	//Move cursor down n lines
-#define ANSI_CUR(n)			"\x1B[nC"	//Move cursor right n lines
-#define ANSI_CUL(n)			"\x1B[nD"	//Move cursor left n lines
-
-#define ANSI_CUP(x,y)		"\x1B[x;yH"	//Move cursor to screen location v,h
-
+//cursor
 #define ANSI_HOME			"\x1B[H"	//Move cursor to upper left corner
-#define ANSI_HVHOME			"\x1B[f"	//Move cursor to upper left corner
 
 #define ANSI_SAVEC			"\x1B7"		//Save cursor position and attributes
 #define ANSI_RESTC			"\x1B8"		//Restore cursor position and attributes
@@ -97,6 +60,59 @@
 #define ANSI_LED2_ON		"\x1B[2q"	//Turn on LED 2
 #define ANSI_LED3_ON		"\x1B[3q"	//Turn on LED 3
 #define ANSI_LED4_ON		"\x1B[4q"	//Turn on LED 4
+
+//constants for moveCursorRelative
+#define ANSI_CUR_UP			'A'
+#define ANSI_CUR_DOWN		'B'
+#define ANSI_CUR_RIGHT		'C'
+#define ANSI_CUR_LEFT		'D'
+
+//constants for moveLine
+#define ANSI_LINE_DOWN		'E'
+#define ANSI_LINE_UP		'F'
+
+//constants for SGR
+#define ANSI_RST_ATTR		0	//Reset all attributes
+#define ANSI_BOLD			1	//Bold font
+#define ANSI_DIM			2	//Dim font
+#define ANSI_ITALIC			3	//Italic font (not widely supported)
+#define ANSI_UNDERLINE		4	//Underline
+#define ANSI_BLINK_S		5	//Slow blink
+#define ANSI_BLINK_F		6	//Fast blink
+#define ANSI_INVERT			7	//Invert video
+#define ANSI_INVISIBLE		8	//Invisible text  (not widely supported)
+#define ANSI_STRIKE			9	//Strike trough font
+
+#define ANSI_BOLD_OFF		21	//Bold font off / double underline ¿?
+#define ANSI_DIM_OFF		22	//Dim font off
+#define ANSI_ITALIC_OFF		23	//Italic font off
+#define ANSI_UNDERLINE_OFF	24	//Underline
+#define ANSI_BLINK_OFF		25	//Slow blink
+#define ANSI_INVERT_OFF		27	//Invert video off
+#define ANSI_INVISIBLE_OFF	28	//Invisible text off (show)
+#define ANSI_STRIKE_OFF		29	//Strike trough font off
+
+//constants for SGR (foreground color)
+#define ANSI_F_BLACK		30	//black
+#define ANSI_F_RED			31	//red
+#define ANSI_F_GREEN		32	//green
+#define ANSI_F_YELLOW		33	//Yellow
+#define ANSI_F_BLUE			34	//Blue
+#define ANSI_F_MAGENTA		35	//Magenta
+#define ANSI_F_CYAN			36	//Cyan
+#define ANSI_F_WHITE		37	//White
+#define ANSI_F_DEFAULT		39	//Default
+
+//constants for SGR (background color)
+#define ANSI_B_BLACK		40	//black
+#define ANSI_B_RED			41	//red
+#define ANSI_B_GREEN		42	//green
+#define ANSI_B_YELLOW		43	//Yellow
+#define ANSI_B_BLUE			44	//Blue
+#define ANSI_B_MAGENTA		45	//Magenta
+#define ANSI_B_CYAN			46	//Cyan
+#define ANSI_B_WHITE		47	//White
+#define ANSI_B_DEFAULT		49	//Default
 
 #endif	/* VT100_H */
 
